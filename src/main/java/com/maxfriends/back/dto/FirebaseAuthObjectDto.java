114acaf446bdeb4d66490d537be0c2efffd83e1d
Max <marchal.maxime@live.fr>
2022-04-11 14:23:16 +0200

    fix(firebase): Connexion firebase OK if à resoudre

--- src/main/java/com/maxfriends/back/dto/FirebaseAuthObjectDto.java
+++ src/main/java/com/maxfriends/back/dto/FirebaseAuthObjectDto.java
@@ -23,4 +23,14 @@ public class FirebaseAuthObjectDto {
 
     String expiresIn;
 
+    public FirebaseAuthObjectDto(String kind, String localId, String email, String displayName, String idToken, boolean registered, String refreshToken, String expiresIn) {
+        this.kind = kind;
+        this.localId = localId;
+        this.email = email;
+        this.displayName = displayName;
+        this.idToken = idToken;
+        this.registered = registered;
+        this.refreshToken = refreshToken;
+        this.expiresIn = expiresIn;
+    }
 }
--- src/main/java/com/maxfriends/back/service/impl/FirebaseServiceImpl.java
+++ src/main/java/com/maxfriends/back/service/impl/FirebaseServiceImpl.java
@@ -1,11 +1,15 @@
 package com.maxfriends.back.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.util.Value;
 import com.maxfriends.back.dto.FirebaseAuthObjectDto;
+import com.maxfriends.back.entity.FirebaseConfiguration;
 import com.maxfriends.back.security.model.LoginUser;
 import com.maxfriends.back.service.IFirebaseService;
 import com.maxfriends.back.utilities.LogsInformations;
-import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.URI;
@@ -13,20 +17,24 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.HashMap;
-import java.util.Properties;
 
+@Service
 public class FirebaseServiceImpl implements IFirebaseService {
 
-    Properties properties = Environment.getProperties();
+    @Autowired
+    private Environment env;
 
     @Override
     public FirebaseAuthObjectDto signWithLoginAndPassword(LoginUser loginUser) throws IOException, InterruptedException {
-        String url = properties.getProperty("firebaseLoginUrl");
-        String key = properties.getProperty("firebaseLoginKey");
+        FirebaseAuthObjectDto firebaseAuthObjectDto = null;
+        String url = env.getProperty("firebase.configuration.firebaseLoginUrl");
+        String key = env.getProperty("firebase.configuration.firebaseLoginKey");
+        String returnSecureToken = env.getProperty("firebase.configuration.firebaseLoginReturnSecureToken");
 
-        HashMap values = new HashMap<String, String>() {{
-            put("name", "John Doe");
-            put ("occupation", "gardener");
+        HashMap<String, String> values = new HashMap<>() {{
+            put("email", loginUser.getUsername());
+            put("password", loginUser.getPassword());
+            put("returnSecureToken", returnSecureToken);
         }};
 
         ObjectMapper objectMapper = new ObjectMapper();
@@ -34,18 +42,19 @@ public class FirebaseServiceImpl implements IFirebaseService {
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
+                .uri(URI.create(url + "?key=" + key))
                 .POST(HttpRequest.BodyPublishers.ofString(requestBody))
                 .build();
 
         HttpResponse<String> response = client.send(request,
                 HttpResponse.BodyHandlers.ofString());
 
-        if(response.statusCode()==200) {
+        if (response.statusCode() == 200) {
             new LogsInformations().affichageLogDate(response.body());
+            //firebaseAuthObjectDto = new FirebaseAuthObjectDto();
         } else {
             new LogsInformations().affichageLogDate(String.valueOf(response.statusCode()));
         }
-        return null;
+        return firebaseAuthObjectDto;
     }
 }
